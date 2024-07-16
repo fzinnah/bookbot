97dea31b17dae5cb1e8fe30ba5a94f613f37c67b
fzinnah <forhadzinnah@gmail.com>
2024-07-16 14:36:47 -0700

    feat: sorts by frequency, removes non alphabetical characters from list and prints

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
-    character_count = count_characters(text)
-    print(character_count)
+    character_count_dict = count_characters(text)
+    character_count_list = list(character_count_dict.items())
+    sorted_list = sorted(character_count_list, key=sort_on, reverse=True)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+    for el in sorted_list:
+        if el[0].isalpha() == True:
+            print(f"The '{el[0]}' was found {el[1]} times")
+    print("--- End Report ---")
+
+def sort_on(list):
+    return list[1]
+
 
 def get_book_text(path):
     with open(path) as f:
