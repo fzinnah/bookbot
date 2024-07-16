0e502f023bfb9fc4e4887242517d9ef09c482bbd
fzinnah <forhadzinnah@gmail.com>
2024-07-16 12:51:22 -0700

    feat: can count freq of characters in document

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
-    # print(text) 
-    print(f"{word_count} words found in the document")
+    character_count = count_characters(text)
+    print(character_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,6 +12,16 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    character_dict = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in character_dict:
+            character_dict[lowered] += 1
+        else:
+            character_dict[lowered] = 1
+    return character_dict
 
 main()
     
