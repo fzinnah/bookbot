dd11e1a21a44c3d5984ecbe7c3ad98f78f27378c
fzinnah <forhadzinnah@gmail.com>
2024-07-16 12:24:22 -0700

    feat: can count words in file

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    word_count = count_words(text)
+    # print(text) 
+    print(f"{word_count} words found in the document")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 main()
     
